d88f39a4fcb4ce568c823e70effa54c281afc5f8
Mateus Cunha <mtscnh2@gmail.com>
2020-04-01 23:04:24 -0300

    Back-End da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+/**
+ * Database setup
+ */
+mongoose.connect(
+    'mongodb://localhost:27017/upload',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }       
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+
+app.use(require('./routes'));
+
+app.listen(3000);
